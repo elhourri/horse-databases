9ff6cfaf9d3ecd7f93919138f49d233590c43355
elhourri <146944553+elhourri@users.noreply.github.com>
2024-03-06 16:35:12 +0100

    Add files via upload

---
+++ cheval.sql
@@ -0,0 +1,7 @@
+select * from nom_de_la_table
+SELECT * FROM chevauxdecurie.cheval WHERE nomRace = 'Arabian';
+SELECT * FROM chevauxdecurie.cheval WHERE nomRace like 'Arabian';
+SELECT poids Type, taille Type FROM chevauxdecurie.race WHERE nomRace ='Arabian';
+SELECT * FROM chevauxdecurie.personne Where fonctionPers like '%proprietaire%' where codeForm = 11;
+SELECT * FROM chevauxdecurie.personne Where numPersSup is null;
+SELECT * FROM chevauxdecurie.concours Where AnneeConcours =2019;
